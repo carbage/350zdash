bd59e7817420c8e9059bb20e11f53614b2aa2ad1
Harwood <aiden.harwood@glencore.co.uk>
2022-06-24 00:29:17 +0100

    Much better

--- protocol.py
+++
@@ -1,4 +0,0 @@
-import obd
-
-ports = obd.scan_serial()
-connection = obd.OBD(portstr=ports[0], protocol="4", baudrate=38400, fast=False)
--- testbed.py
+++ testbed.py
@@ -8,32 +8,42 @@ import websockets
 debug = True
 
 logs = {
-    'dtc': obd.commands.GET_DTC,
-    'rpm': obd.commands.RPM,
-    'speed': obd.commands.SPEED
+    'dtc': [obd.commands.GET_DTC, 0, 0],
+    'rpm': [obd.commands.RPM, 0, 8000],
+    'speed': [obd.commands.SPEED, 0, 180],
+    'temp': [obd.commands.COOLANT_TEMP, 0, 140],
+    'fuel': [obd.commands.FUEL_LEVEL, 0, 100]
 }
 
 def init():
-    #obd.logger.removeHandler(obd.console_handler)
+    obd.logger.removeHandler(obd.console_handler)
     ports = obd.scan_serial()
+    if not debug:
+        print(ports)
     return obd.OBD(portstr=ports[0], protocol="4", baudrate=38400, fast=False)
  
 async def handler(websocket):
     print("Received incoming connection fron frontend...")
+    percent = 0
     while True:
         try:
+            if percent >= 100:
+                percent = 0
+            else:
+                percent += 5
             data = {}
             for item in logs:
                 command = logs[item]
                 if debug:
-                    data[item] = int(random() * 200) + 1000
+                    data[item] = int((percent/100) * logs[item][2])
                 else:
                     if connection.supports(command):
-                        data[item] = connection.query(command).value
+                        data[item] = str(connection.query(command).value)
 
             if debug:
                 print("Sending: ", data)
             await websocket.send(json.dumps(data))
+            await asyncio.sleep(0.25)
         except websockets.ConnectionClosedOK:
             break
 
