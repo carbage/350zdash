3b2b6106914966b22f982183249c1b325edd043a
Aiden <awaitingvalhalla@gmail.com>
2022-06-24 12:32:45 +0100

    Better data sanitization

--- testbed.py
+++ testbed.py
@@ -5,21 +5,21 @@ import obd
 import asyncio
 import websockets
 
-debug = True
+from obd import Unit
+
+debug = False
 
 logs = {
     'dtc': [obd.commands.GET_DTC, 0, 0],
     'rpm': [obd.commands.RPM, 0, 8000],
     'speed': [obd.commands.SPEED, 0, 180],
     'temp': [obd.commands.COOLANT_TEMP, 0, 140],
-    'fuel': [obd.commands.FUEL_LEVEL, 0, 100]
+    'fuel': [obd.commands.COOLANT_TEMP, 0, 100]
 }
 
 def init():
     obd.logger.removeHandler(obd.console_handler)
     ports = obd.scan_serial()
-    if not debug:
-        print(ports)
     return obd.OBD(portstr=ports[0], protocol="4", baudrate=38400, fast=False)
  
 async def handler(websocket):
@@ -33,17 +33,25 @@ async def handler(websocket):
                 percent += 5
             data = {}
             for item in logs:
-                command = logs[item]
+                command = logs[item][0]
                 if debug:
                     data[item] = int((percent/100) * logs[item][2])
                 else:
+                    # data[item] = str(connection.query(command).value)
                     if connection.supports(command):
-                        data[item] = str(connection.query(command).value)
+                        res = connection.query(command).value
+                        if isinstance(res, Unit.Quantity):
+                            data[item] = res.magnitude
+                        else:
+                            data[item] = str(res)
+                    else:
+                        data[item] = str(0)
 
             if debug:
                 print("Sending: ", data)
             await websocket.send(json.dumps(data))
-            await asyncio.sleep(0.25)
+            if debug:
+                await asyncio.sleep(0.25)
         except websockets.ConnectionClosedOK:
             break
 
